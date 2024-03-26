ac3fd1ae0db91710a7134dad3584f42951c2ec99
hi <hi@DESKTOP-6S74FBO>
2024-03-26 09:58:20 +0530

    commit program

---
+++ RKITHUB/src/com/rkithub/com/Sampledemo.java
@@ -0,0 +1,10 @@
+package com.rkithub.com;
+
+public class Sampledemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world");
+	}
+
+}
