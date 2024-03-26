bf28a6568b82c913c18835413721f229fde88733
hi <hi@DESKTOP-6S74FBO>
2024-03-26 10:08:32 +0530

    BRANCH EXAMPLE

---
+++ RKITHUB/src/com/badari/com/AddDemo.java
@@ -0,0 +1,15 @@
+package com.badari.com;
+
+public class AddDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int x, y;
+     x= 5;
+     y=7;
+     System.out.println(x+y);
+     
+     
+	}
+
+}
